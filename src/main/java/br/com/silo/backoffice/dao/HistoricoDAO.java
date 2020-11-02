c2f8211503fb1aed3e0705a9d42151b53114a47c
daniMend <50723675+daniMend@users.noreply.github.com>
2020-11-01 21:56:12 -0300

    Coluna Equipamento adicionada em Historico

--- src/main/java/br/com/silo/backoffice/dao/HistoricoDAO.java
+++ src/main/java/br/com/silo/backoffice/dao/HistoricoDAO.java
@@ -17,12 +17,12 @@ public interface HistoricoDAO extends PagingAndSortingRepository<Historico, Long
     @Query(nativeQuery = true, value = "SELECT h FROM historico h WHERE h.datahora = ?1")
     Historico findByDate(String datahora);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM historico h JOIN veiculo v ON h.veiculo_id=v.id WHERE datahora LIKE concat('%', ?1,'%')")
+    @Query(nativeQuery = true, value = "SELECT * FROM historico h JOIN veiculo v ON h.veiculo_id=v.id JOIN equipamento q ON  v.equipamento_id=q.id WHERE datahora LIKE concat('%', ?1,'%')")
     Page<Historico> findPaged(String datahora, Pageable pageable);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM historico h JOIN veiculo v ON h.veiculo_id=v.id WHERE v.empresa_id = ?1")
+    @Query(nativeQuery = true, value = "SELECT * FROM historico h JOIN veiculo v ON h.veiculo_id=v.id JOIN equipamento q ON  v.equipamento_id=q.id WHERE v.empresa_id = ?1")
     List<Historico> findGeral(long empresaId);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM historico h JOIN veiculo v ON h.veiculo_id=v.id WHERE v.empresa_id =?1 AND v.id=?1 ORDER BY h.id DESC LIMIT 1;")
+    @Query(nativeQuery = true, value = "SELECT * FROM historico h JOIN veiculo v ON h.veiculo_id=v.id JOIN equipamento q ON  v.equipamento_id=q.id WHERE v.empresa_id =?1 AND v.id=?1 ORDER BY h.id DESC LIMIT 1;")
     Historico findStatus(long empresaId,long veiculoId);
 }
